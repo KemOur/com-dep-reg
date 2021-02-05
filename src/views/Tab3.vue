5a171e64e78341ee5ee4ea6f50568e71f1438233
Junior <duartejusanca@gmail.com>
2021-02-06 00:55:43 +0100

    communes:ok

---
+++ src/views/Tab3.vue
@@ -0,0 +1,34 @@
+<template>
+  <ion-page>
+    <ion-header>
+      <ion-toolbar>
+        <ion-title style="text-align: center">RR</ion-title>
+      </ion-toolbar>
+    </ion-header>
+
+    <ion-item>
+      <ion-label position="floating">Search region</ion-label>
+      <ion-input></ion-input>
+    </ion-item>
+
+    <ion-content :fullscreen="true">
+      <ion-header collapse="condense">
+        <ion-toolbar>
+          <ion-title size="large">Tab 3</ion-title>
+        </ion-toolbar>
+      </ion-header>
+      
+      <ExploreContainer name="Tab 3 page" />
+    </ion-content>
+  </ion-page>
+</template>
+
+<script>
+import { IonPage, IonHeader, IonToolbar, IonTitle, IonContent } from '@ionic/vue';
+import ExploreContainer from '@/components/ExploreContainer.vue';
+
+export default  {
+  name: 'Tab3',
+  components: { ExploreContainer, IonHeader, IonToolbar, IonTitle, IonContent, IonPage }
+}
+</script>
---
+++ src/views/Tabs.vue
@@ -0,0 +1,42 @@
+<template>
+  <ion-page>
+    <ion-tabs>
+      <ion-tab-bar slot="bottom">
+        <ion-tab-button tab="tab1" href="/tabs/tab1">
+          <ion-icon :icon="businessOutline" />
+          <ion-label>communes</ion-label>
+        </ion-tab-button>
+
+        <ion-tab-button tab="tab2" href="/tabs/tab2">
+          <ion-icon :icon="busOutline" />
+          <ion-label>departments</ion-label>
+        </ion-tab-button>
+        
+        <ion-tab-button tab="tab3" href="/tabs/tab3">
+          <ion-icon :icon="bicycleOutline" />
+          <ion-label>regions</ion-label>
+        </ion-tab-button>
+      </ion-tab-bar>
+    </ion-tabs>
+  </ion-page>
+</template>
+
+<script>
+import { IonTabBar, IonTabButton, IonTabs, IonLabel, IonIcon, IonPage } from '@ionic/vue';
+import { ellipse, square, businessOutline, busOutline, bicycleOutline } from 'ionicons/icons';
+
+export default {
+  name: 'Tabs',
+  components: { IonLabel, IonTabs, IonTabBar, IonTabButton, IonIcon, IonPage },
+  setup() {
+    return {
+      ellipse, 
+      square,
+      businessOutline,
+      busOutline,
+      bicycleOutline,
+
+    }
+  }
+}
+</script>
